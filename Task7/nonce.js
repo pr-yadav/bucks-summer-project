386d2d5849679f40d69ffe41ba7fc5bf85adcb20
Priyanshu Yadav <56887107+pr-yadav@users.noreply.github.com>
2020-07-02 11:38:54 +0530

    Update nonce.js

--- Task7/nonce.js
+++ Task7/nonce.js
@@ -3,16 +3,19 @@ const { Worker, parentPort } = require('worker_threads');
 
 
 var i=0;
-var data = "hellp";
+var data = "";
 parentPort.on('message',msg =>{
-    data=msg;
-})
+data=msg;
+//console.log(data)
 while(1){
     var result=sha256(data+i);
     var hex= parseInt("0x"+result);
     if(hex<0x0000000F00000000000000000000000000000000000000000000000000000000){
+        console.log(result);
+        console.log(string+i);
         parentPort.postMessage(i);
         break;
     }
     i++;
 }
+})
